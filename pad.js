87ebe94f620ad7d8ef11472d06bcbaff9fd56dca
mpizza <mmm198219@gmail.com>
2012-07-16 18:03:22 +0800

    add ratio

--- pad.js
+++ pad.js
@@ -18,7 +18,16 @@ var imagepad={
   },
   
   drawimg: function(){
-    this.hidectx.drawImage(this.img,0,0);
+    var bw=parseInt(document.body.clientWidth);
+    var Iw=parseInt(this.img.width);
+    if(Iw<bw){
+      this.hidectx.drawImage(this.img,0,0);
+    }else{
+      var ratio=bw/Iw;
+      canvas_width = this.img.width * ratio;
+      canvas_height = this.img.height * ratio;
+      ctx.drawImage(canvasCopy, 0, 0, this.hidepad.width, this.hidepad.height, 0, 0,canvas_width, canvas_height);
+    }
   },
   
   showpad: function(frame){

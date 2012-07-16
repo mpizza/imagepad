6ab96a6156ffcb36ba7e9d79579b9b428471eac2
mpizza <mmm198219@gmail.com>
2012-07-16 15:24:45 +0800

    add touch

--- pad.js
+++ pad.js
@@ -172,7 +172,7 @@ $(function(){
   
   getpad.pad.addEventListener("touchstart", function(e){
     getpad.pen=1;
-    getpos=posfn(e, getpad.pad);
+    getpos=posfn(evt.changedTouches, getpad.pad);
     getpad.creatLine(getpos);
   });
   
@@ -185,7 +185,7 @@ $(function(){
   
   getpad.pad.addEventListener("touchmove", function(e){
     if(getpad.pen==1){  
-      getpos=posfn(e, getpad.pad);
+      getpos=posfn(evt.changedTouches, getpad.pad);
       getpad.drawLine(getpos);
     }
   });

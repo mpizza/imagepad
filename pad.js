09fb98c16a270a16d2dbbb611ee0d1f31ea83584
mpizza <mmm198219@gmail.com>
2012-07-16 18:11:32 +0800

    fixed bug

--- pad.js
+++ pad.js
@@ -24,8 +24,8 @@ var imagepad={
       this.hidectx.drawImage(this.img,0,0);
     }else{
       var ratio=bw*0.8/Iw;
-      canvas_width = this.img.width * ratio;
-      canvas_height = this.img.height * ratio;
+      var canvas_width = this.img.width * ratio;
+      var canvas_height = this.img.height * ratio;
       this.hidectx.drawImage(this.img, 0, 0, this.hidepad.width, this.hidepad.height, 0, 0,canvas_width, canvas_height);
     }
   },
